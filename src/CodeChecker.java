6f326b215661ae6eaf1e3a0f6afbcd6c58304d16
DoudouJack <doudoujack.nancy@gmail.com>
2019-06-21 17:49:15 +0200

    -Added login function -Edited CodeChecker to login on first run, get AccessToken and use it until it expires, then relogin

--- src/CodeChecker.java
+++ src/CodeChecker.java
@@ -3,18 +3,20 @@ import org.json.JSONObject;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
-import java.net.ProtocolException;
 import java.net.URL;
 
 
 /**
  * @author Edouard
  * Class that connects to the API via SSL and gets the PIN code from the actual booking
- * If no booking, 404. TODO handle an empty booking
+ * When starting the program the AccessToken is empty. It logs in to the API and gets the Access Token. It is then used
+ * for further requests. If a GET request receives a Login Error, then the login method is called to get the new AccessToken
+ * If no current booking
  */
 public class CodeChecker {
 
     public String capsuleCode;
+    private String actualAccessToken;
     //public boolean internet;
 /*
     public CodeChecker(){
@@ -22,6 +24,55 @@ public class CodeChecker {
         getCurrentCode();
     }*/
 
+private String login(){
+    try{
+
+        //TODO check with Fabian for authentication when already connected. For now it throws an error because already authenticated
+        //TODO goal here is to authenticate each time in order to get the actual AccessToken
+        //DAS HIER IST MIT DEM POST DINGS
+        URL auth = new URL("https://platania.info:3000/api/SnoozeUsers/login");
+
+        JSONObject post = new JSONObject();
+        post.put("username","Capsule1User");
+        post.put("password","0000");
+
+        HttpsURLConnection conn1 = (HttpsURLConnection) auth.openConnection();
+        SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        conn1.setSSLSocketFactory(socketFactory);
+        conn1.setDoOutput(true);
+        conn1.setRequestMethod("POST");
+        conn1.addRequestProperty("content-type","application/json");
+        OutputStream os = conn1.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+        String params = post.toString();
+        osw.write(params);
+        osw.flush();
+        osw.close();
+        os.close();
+        conn1.connect();
+
+        String result3;
+        BufferedInputStream bis = new BufferedInputStream(conn1.getInputStream());
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        int result2 = bis.read();
+        while(result2 != -1) {
+            buf.write((byte) result2);
+            result2 = bis.read();
+        }
+        //result3 = buf.toString();
+
+        //System.out.println(buf.toString());
+
+        JSONObject login = new JSONObject(buf.toString().replaceAll("[\\[\\]]", ""));
+        actualAccessToken = login.getString("id");
+        //System.out.println(actualAccessToken);
+
+
+    } catch (IOException e){
+        e.printStackTrace();
+    }
+    return actualAccessToken;
+}
 
     /**
      * Gets the "Pin" from the JSON got in the GET request. Returns it.
@@ -29,49 +80,10 @@ public class CodeChecker {
      */
     public String getCurrentCode(){
         try {
-/*
-            //TODO check with Fabian for authentication when already connected. For now it throws an error because already authenticated
-            //TODO goal here is to authenticate each time in order to get the actual AccessToken
-            //DAS HIER IST MIT DEM POST DINGS
-            URL auth = new URL("https://platania.info:3000/api/SnoozeUsers/login");
-
-            JSONObject post = new JSONObject();
-            post.put("username","Capsule1User");
-            post.put("password","0000");
-
-            HttpsURLConnection conn1 = (HttpsURLConnection) auth.openConnection();
-            SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            conn1.setSSLSocketFactory(socketFactory);
-            conn1.setDoOutput(true);
-            conn1.setRequestMethod("POST");
-            OutputStream os = conn1.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-            conn1.addRequestProperty("content-type","application/json");
-            String params = post.toString();
-            osw.write(params);
-            osw.flush();
-            osw.close();
-            os.close();
-            conn1.connect();
-
-            String result3;
-            BufferedInputStream bis = new BufferedInputStream(conn1.getInputStream());
-            ByteArrayOutputStream buf = new ByteArrayOutputStream();
-            int result2 = bis.read();
-            while(result2 != -1) {
-                buf.write((byte) result2);
-                result2 = bis.read();
-            }
-            result3 = buf.toString();
-            System.out.println(result3);
-
-*/
-
 //TODO mit connection.getErrorStream() get the JSON error content (this is only for beauty purposes, actual code works)
 
-
-            //URL url = new URL("https://platania.info:3000/api/Capsules/2/bookings?access_token=GN0tME3nUBa6auETCDju80cAzMSMDaDY791UafudXydp6AwwLfVjEJDDxJTjHEg3");
-            URL url = new URL("https://platania.info:3000/api/Capsules/getCurrentBooking?access_token=zKBMRgGb6o7Fskq9wiy9nzKT3xgYqpI9BGjJTspxkOrPAJohVaS4iaCHas1AS7IY");
+            URL baseURL = new URL("https://platania.info:3000/api/Capsules/getCurrentBooking?access_token=");
+            URL url = new URL(baseURL+actualAccessToken);
             HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
             SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
             conn.setSSLSocketFactory(socketFactory);
@@ -82,6 +94,9 @@ public class CodeChecker {
                     capsuleCode = "No booking";
             } else if (conn.getResponseCode() == 401){
                 capsuleCode = "Login Error";
+                login();
+                getCurrentCode();
+                    System.out.println("after login error, login then get new access token: "+actualAccessToken);
             } else if (conn.getResponseCode() != 200) {
                 throw new RuntimeException("Failed : HTTP error code : "
                         + conn.getResponseCode());
