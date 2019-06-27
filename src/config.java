a898dd6ea514c4db4acd46945c8fa8abf1ae36f3
phillip <phillip.ertel@googlemail.com>
2019-06-28 01:47:29 +0200

    generated meta-inf for generating jars and updated paths for scripts

--- src/config.java
+++ src/config.java
@@ -3,7 +3,7 @@
  * Interface holds paths for easy acces in case they need to be changed
  */
 public interface config {
-    String pythonPath = "python3"; // to run the script(s) as python3 set to "python3", to run as python 2 type "python"
-    String sensorScriptPath = "/home/marcy/Schreibtisch/testScript.py"; // path to the script which checks for sensor input
-    String openScriptPath = "openDoor.py"; // path to the script which opens the door
+    String pythonPath = "python"; // to run the script(s) as python3 set to "python3", to run as python 2 type "python"
+    String sensorScriptPath = "/etc/Snooze/sensor.py"; // path to the script which checks for sensor input
+    String openScriptPath = "/etc/Snooze/open.py"; // path to the script which opens the door
 }
